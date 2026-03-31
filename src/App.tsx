b1f333a2663fc9f040df431ef28c3278456609df
Youkow69 <lucascrds111008@gmail.com>
2026-03-31 19:04:29 +0200

    fix: remove unused countCompletedDailyQuests import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect, useMemo, useState } from 'react'
 import { foods, programs, savedMeals } from './data'
 import {
   calculateTargets,
-  countCompletedDailyQuests,
   formatNumber,
   generateMainObjectives,
   getAdaptiveTDEEStatus,

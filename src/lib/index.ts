5a158a1a0755c8730ffdc2bbd7a842e056e104b4
Youkow69 <lucascrds111008@gmail.com>
2026-04-02 02:22:53 +0200

    refacto: extract lib/index domain module

---
+++ src/lib/index.ts
@@ -0,0 +1,7 @@
+export * from './dates'
+export * from './nutrition'
+export * from './training'
+export * from './progression'
+export * from './gamification'
+export * from './recommendations'
+export * from './utils'

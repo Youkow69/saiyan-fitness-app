5dc6da4241a4043ba6809997c3776c9830fb97d4
Youkow69 <lucascrds111008@gmail.com>
2026-04-02 18:01:55 +0200

    fix: remove unused React import in WorkoutHistory

--- src/components/stats/WorkoutHistory.tsx
+++ src/components/stats/WorkoutHistory.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react'
+import { useState, useMemo } from 'react'
 import type { WorkoutLog } from '../../types'
 import { getExerciseById, getWorkoutVolume, formatNumber } from '../../lib'
 
